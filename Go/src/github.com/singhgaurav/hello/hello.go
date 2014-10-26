bbd442f12e04e57afadb6bc18d097676d19127f6
Gaurav Singh <gaurav.visen@gmail.com>
2014-10-27 00:36:05 +0530

    added package and some test code

--- Go/src/github.com/singhgaurav/hello/hello.go
+++ Go/src/github.com/singhgaurav/hello/hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/singhgaurav/mathutil"
+)
 
 func main() {
-	fmt.Printf("hello, world\n")
+	fmt.Printf("Hello, World. Sqrt(2) :  %v\n", mathutil.Sqrt(2))
 }
